96ee1f7412ba3357e69f2ac5ec4c3c2983582992
Zvi Benazar <jzvi12@aol.com>
2023-06-04 15:00:14 -0400

    Update episode.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-# Guarantee that we are on the same path as our *.js scripts
-cd /
-npm ci  # more suitable for installs from scratch 
-npm start
+npm install
+sudo npm i puppeteer --unsafe-perm=true --allow-root
+node index.js
